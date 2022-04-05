8223c6af8be0acce7a025fda4548231fb239b35c
mishozz <mzlatev345@gmai.com>
2022-04-05 18:50:07 +0300

    test

--- fmi-2022-03-react-todos/src/components/TodoList.js
+++ fmi-2022-03-react-todos/src/components/TodoList.js
@@ -4,20 +4,12 @@ import PropTypes from 'prop-types'
 import { ALL_STATUSES, ACTIVE, CANCELED, COMPLETED } from '../model/todo-model'
 import TodoItem from './TodoItem'
 
-<<<<<<< HEAD
 
 
 const TodoList = ({ todos,filter, ...props }) => {
     return (
         <ul className="TodoList-items">
             {todos.filter(todo => todo.status === filter || filter === ALL_STATUSES )
-=======
-const TodoList = ({ todos, filter, ...props }) => {
-    return (
-        <ul className="TodoList-items">
-            {todos
-            .filter(todo => todo.status === filter || filter === ALL_STATUSES)
->>>>>>> 87908187112f5b25aac414cea7c5564cbb6bfdd9
             .map(todo => (
                 <TodoItem key={todo.id} todo={todo} {...props} />
             ))}
