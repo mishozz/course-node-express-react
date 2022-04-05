58b1f8db2e50e5b84fc3f73cb2a600bbdc7eb369
mishozz <mzlatev345@gmai.com>
2022-04-05 18:36:38 +0300

    Merge

--- fmi-2022-03-react-todos/src/App.js
+++ fmi-2022-03-react-todos/src/App.js
@@ -4,9 +4,12 @@ import TodoFilter from './components/TodoFilter';
 import TodoInput from './components/TodoInput';
 import TodoList from './components/TodoList';
 import MOCK_TODOS from './model/mock-todos';
+import { ALL_STATUSES, ACTIVE, CANCELED, COMPLETED } from './model/todo-model'
 
 function App() {
   const [todos, setTodos] = useState(MOCK_TODOS)
+  const [filter, setFiter] = useState(ALL_STATUSES)
+
   function deleteTodo(deleted) {
     setTodos(oldTodos => oldTodos.filter(td => td.id !== deleted.id));
   }
@@ -14,14 +17,15 @@ function App() {
     setTodos(oldTodos => oldTodos.map(td => td.id === changedTodo.id? changedTodo : td))
   }
   function filterChanged(filterText){
-    //You code here
+    console.log(filterText)
+    setFiter(filterText)
   }
   return (
     <div className="App-header">
       <h1>React TODOs Demo</h1>
       <TodoInput onSubmitTodo={todo => setTodos(oldTodos => [...oldTodos, todo])} />
-      <TodoFilter onFilterChange={filterChanged} />
-      <TodoList todos={todos} onDelete={deleteTodo} onStatusChange={statusChanged}/>
+      <TodoFilter filter={filter} onFilterChange={filterChanged} />
+      <TodoList todos={todos} filter={filter} onDelete={deleteTodo} onStatusChange={statusChanged}/>
     </div>
   );
 }
--- fmi-2022-03-react-todos/src/components/TodoFilter.js
+++ fmi-2022-03-react-todos/src/components/TodoFilter.js
@@ -1,12 +1,25 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { ACTIVE, ALL_STATUSES, CANCELED, COMPLETED } from '../model/todo-model';
 
-const TodoFilter = props => {
+const TodoFilter = ({filter,onFilterChange}) => {
+  function filterChange(event) {
+    onFilterChange(Number.parseInt(event.target.value))
+  }
   return (
-    <div>TodoFilter: YOUR CODE HERE! (ALL_STATUSES = 0, ACTIVE = 1, COMPLETED = 2, CANCELED = 3) </div>
+    // <div>TodoFilter: YOUR CODE HERE! (ALL_STATUSES = 0, AALL_STATUSES = 1, COMPLETED = 2, CANCELED = 3) </div>
+    <select value={filter} onChange={filterChange}>
+            <option value={ALL_STATUSES}>All</option>
+            <option value={ACTIVE}>Active</option>
+            <option value={COMPLETED}>Completed</option>
+            <option value={CANCELED}>Canceled</option>
+    </select>
   )
 }
 
-TodoFilter.propTypes = {}
+TodoFilter.propTypes = {
+  onFilterChange: PropTypes.func.isRequired,
+  filter: PropTypes.number.isRequired,
+}
 
 export default TodoFilter
